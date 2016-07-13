cdada1e8398724f5476cee2f689feaebd0fa0591
iryb <iryb@live.ru>
2016-07-13 14:46:01 +0200

    add headers to custom response

--- custom_reponse.py
+++ custom_reponse.py
@@ -4,6 +4,7 @@ from requests.status_codes import codes
 class CustomResponse(object):
     _status_code = codes.ok
     _text = ''
+    _headers = []
 
     @property
     def text(self):
@@ -13,13 +14,18 @@ class CustomResponse(object):
     def status_code(self):
         return self._status_code
 
-    def __init__(self, text='', status_code=codes.ok):
+    @property
+    def headers(self):
+        return self._headers
+
+    def __init__(self, text='', status_code=codes.ok, headers=[]):
         self._status_code = status_code
         self._text = text
+        self._headers = headers
 
     def __str__(self):
-        return "status_code: %s, text: %s" % (self._status_code, self._text)
+        return "status_code: %s, text: %s, headers: %s" % (self._status_code, self._text, self.headers)
 
     def to_flask_response(self):
-        from flask import Response as FlaskResponse
-        return FlaskResponse(self._text, self._status_code)
+        from flask import make_response
+        return make_response((self._text,self._status_code, self._headers))
