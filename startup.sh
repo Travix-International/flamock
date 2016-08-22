3c676a9ffe63a70aa8e3546bbb44177a8665a013
iryb <iryb@live.ru>
2016-08-22 18:15:33 +0200

    update startup script for expectatios

--- startup.sh
+++ startup.sh
@@ -4,8 +4,9 @@
 # PROXY_HOST="www.google.nl";
 # PROXY_HEADERS="header1=value1;header2=value2";
 # PORTS="8801 8802 8805"
+# EXPECTATIONS="[{\"key\":\"fwd8801\",\"request\":{\"headers\":{\"X-Incoming-Port\":\"8801\"}},\"forward\":{\"scheme\":\"http\",\"host\":\"google.com:8801\"},\"priority\":0},{\"key\":\"fwd8802\",\"request\":{\"headers\":{\"X-Incoming-Port\":\"8802\"}},\"forward\":{\"scheme\":\"http\",\"host\":\"google.com:8802\"},\"priority\":0},{\"key\":\"fwd8805\",\"request\":{\"headers\":{\"X-Incoming-Port\":\"8805\"}},\"forward\":{\"scheme\":\"http\",\"host\":\"google.com:8805\"},\"priority\":0}]"
 
-echo "Env variables: PROXY_SCHEME=$PROXY_SCHEME, PROXY_HOST=$PROXY_HOST, PROXY_HEADERS=$PROXY_HEADERS, PORTS=$PORTS"
+echo "Env variables: PROXY_SCHEME=$PROXY_SCHEME, PROXY_HOST=$PROXY_HOST, PROXY_HEADERS=$PROXY_HEADERS, PORTS=$PORTS, EXPECTATIONS=$EXPECTATIONS"
 
 args=""
 if [ -n "$PROXY_HOST" ]; then
@@ -18,6 +19,10 @@ fi
 
 if [ -n "$PROXY_HEADERS" ]; then
     args=$args" --proxy_headers $PROXY_HEADERS"
+fi
+
+if [ -n "$EXPECTATIONS" ]; then
+    args=$args" --expectations $EXPECTATIONS"
 fi
 
 if [ -n "$PORTS" ]; then
