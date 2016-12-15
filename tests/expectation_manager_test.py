e04a42f175a85b528d04589d3cab28103cc3bead
Igor Rybak <iryb@live.ru>
2016-12-15 01:46:05 +0100

    move to dynamic classes

--- tests/expectation_manager_test.py
+++ tests/expectation_manager_test.py
@@ -10,53 +10,54 @@ logging.basicConfig(level=logging.DEBUG, format=logging_format)
 
 class ExpectationManagerTest(unittest.TestCase):
 
+    _expectation_manager = None
+
     def setUp(self):
-        ExpectationManager.expectations.clear()
+        self._expectation_manager = ExpectationManager()
 
     def test_010_add(self):
         exp = {'request': {'path': 'pathv'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
-        self.assertEqual(len(ExpectationManager.expectations), 1)
+        self.assertEqual(len(self._expectation_manager.get_expectations()), 1)
 
-        for key, value in ExpectationManager.expectations.items():
+        for key, value in self._expectation_manager.get_expectations().items():
             self.assertEqual(exp, value)
 
     def test_020_remove(self):
         exp = {'request': {'path': 'pathv1'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
         exp = {'request': {'path': 'pathv2'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 2)
 
         for key, value in items:
-            resp = ExpectationManager.remove({'key': key})
+            resp = self._expectation_manager.remove({'key': key})
             self.assertEquals(200, resp.status_code)
             break
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 1)
 
     def test_030_remove_all(self):
         exp = {'request': {'path': 'pathv1'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
         exp = {'request': {'path': 'pathv2'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 2)
 
-        resp = ExpectationManager.remove_all()
-        self.assertEquals(200, resp.status_code)
-        items = ExpectationManager.get_expectations_as_dict().items()
+        self._expectation_manager.clear()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 0)
 
     def test_040_json_to_dict_positive(self):
         exp = {'request': {'path': 'pathv1'}, 'response': {'httpcode': 200, 'body': 'Mock answer!'}}
-        exp_dict, resp = ExpectationManager.json_to_dict(json.dumps(exp))
+        exp_dict, resp = self._expectation_manager.json_to_dict(json.dumps(exp))
         self.assertEquals(200, resp.status_code)
         self.assertEquals(exp_dict['request'], exp['request'])
         self.assertEquals(exp_dict['response']['httpcode'], exp['response']['httpcode'])
@@ -64,34 +65,34 @@ class ExpectationManagerTest(unittest.TestCase):
 
     def test_050_json_to_dict_negative(self):
         exp = "{'request': {'path': 'pathv1'}, 'response': {'httpcode': 200, 'body': 'Mock answer!'}}"
-        exp_dict, resp = ExpectationManager.json_to_dict(str(exp))
+        exp_dict, resp = self._expectation_manager.json_to_dict(str(exp))
         self.assertEquals(400, resp.status_code)
         self.assertEquals(exp_dict, None)
 
     def test_060_add_expectation_with_key(self):
         custom_key = 'custom_key'
         exp = {'key': custom_key, 'request': {'path': 'pathv1'}, 'response': {'httpcode': 200, 'body': "Mock answer!"}}
-        resp = ExpectationManager.add(exp)
+        resp = self._expectation_manager.add(exp)
         self.assertEquals(200, resp.status_code)
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 1)
 
-        resp = ExpectationManager.remove({'key': custom_key})
+        resp = self._expectation_manager.remove({'key': custom_key})
         self.assertEquals(200, resp.status_code)
 
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 0)
 
     def test_070_update_existing_expectation(self):
         exp1 = {'key': 'custom_key', 'request': {'path': 'pathv1'}}
-        resp = ExpectationManager.add(exp1)
+        resp = self._expectation_manager.add(exp1)
         self.assertEquals(200, resp.status_code)
 
         exp2 = {'key': 'custom_key', 'request': {'path': 'pathv2'}}
-        resp = ExpectationManager.add(exp2)
+        resp = self._expectation_manager.add(exp2)
         self.assertEquals(200, resp.status_code)
 
-        items = ExpectationManager.get_expectations_as_dict().items()
+        items = self._expectation_manager.get_expectations().items()
         self.assertEqual(len(items), 1)
 
         for key, value in items:
--- tests/flamock_integration_test.py
+++ tests/flamock_integration_test.py
@@ -3,8 +3,7 @@ import logging
 import unittest
 from flamock import logging_format
 from flamock import admin_path as flamock_admin_path
-from flamock import app as flamock_app
-
+from flamock import app
 from expectation_manager import ExpectationManager
 from response_manager import ResponseManager
 
@@ -17,21 +16,20 @@ class FlamockTest(unittest.TestCase):
     base_host = "0.0.0.0"
     base_url = 'http://%s:%s' % (base_host, base_port)
 
-    def setUp(self):
-        flamock_app.config['TESTING'] = True
-        self.app = flamock_app.test_client()
-        self.app.set_cookie('localhost', 'cookie1', 'cookie1_value')
-        self.app.set_cookie('localhost', 'cookie2', 'cookie2_value')
-        ExpectationManager.expectations.clear()
-        ResponseManager.host_whitelist = ["0.0.0.0"]
-        ResponseManager.log_container.clean()
+    @classmethod
+    def setUpClass(cls):
+        app.config['TESTING'] = True
+        app.response_manager.host_whitelist = ["0.0.0.0"]
 
-    def tearDown(self):
-        self.app.delete_cookie('localhost', 'cookie1')
-        self.app.delete_cookie('localhost', 'cookie2')
+    def setUp(self):
+        app.response_manager.clear_log_messages()
+        app.expectation_manager.clear()
+        self.client = app.test_client()
+        self.client.set_cookie('localhost', 'cookie1', 'cookie1_value')
+        self.client.set_cookie('localhost', 'cookie2', 'cookie2_value')
 
     def test_010_check_status(self):
-        resp = self.app.get(self.base_url + '/' + flamock_admin_path + '/status')
+        resp = self.client.get(self.base_url + '/' + flamock_admin_path + '/status')
 
         self.assertEqual(resp.status_code, 200)
         self.assertEquals('OK', resp.get_data(as_text=True))
@@ -39,8 +37,8 @@ class FlamockTest(unittest.TestCase):
     def test_020_no_expectation_get_headers_and_cookies(self):
         path = 'a/b/c'
 
-        resp = self.app.get(self.base_url + '/' + path,
-                            headers={'header1': 'header1_value', 'header2': 'header2_value'})
+        resp = self.client.get(self.base_url + '/' + path,
+                               headers={'header1': 'header1_value', 'header2': 'header2_value'})
 
         self.assertEqual(resp.status_code, 200)
         resp_text = resp.get_data(as_text=True)
@@ -77,23 +75,23 @@ class FlamockTest(unittest.TestCase):
             'priority': 0
         }
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_fwd))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_fwd))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_resp))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_resp))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.get(self.base_url + '/folder/service.aspx',
-                            data='<session_id>1234</session_id>',
-                            headers={'header1': 'header1_value', 'header2': 'header2_value'})
+        resp = self.client.get(self.base_url + '/folder/service.aspx',
+                               data='<session_id>1234</session_id>',
+                               headers={'header1': 'header1_value', 'header2': 'header2_value'})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.get_data(as_text=True), 'Mock answer!')
 
-        resp = self.app.get(self.base_url + '/folder/service.aspx',
-                            data='<session_id>456</session_id>',
-                            headers={'header1': 'header1_value', 'header2': 'header2_value'})
+        resp = self.client.get(self.base_url + '/folder/service.aspx',
+                               data='<session_id>456</session_id>',
+                               headers={'header1': 'header1_value', 'header2': 'header2_value'})
         self.assertEqual(resp.status_code, 400)
 
     def test_040_wide_expectation_with_empty_path(self):
@@ -101,21 +99,17 @@ class FlamockTest(unittest.TestCase):
         fwd_scheme = 'https'
 
         exp_fwd = {
-            'request': {
-                'path': '.*'
-            },
             'forward': {
                 'scheme': fwd_scheme,
                 'host': fwd_host
-            },
-            'priority': 0
+            }
         }
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_fwd))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_fwd))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.get(self.base_url + '/')
+        resp = self.client.get(self.base_url + '/')
         self.assertEqual(resp.status_code, 200)
         self.assertIn('>Google<', resp.get_data(as_text=True))
 
@@ -132,9 +126,6 @@ class FlamockTest(unittest.TestCase):
             'priority': 1
         }
         exp_mock_all = {
-            'request': {
-                'path': '.*'
-            },
             'response': {
                 'httpcode': 200,
                 'body': "Mock answer without header!"
@@ -142,18 +133,18 @@ class FlamockTest(unittest.TestCase):
             'priority': 0
         }
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_mock_header))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_mock_header))
         self.assertEqual(resp.status_code, 200)
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_mock_all))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_mock_all))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.get(self.base_url + '/', headers={'sid': '123'})
+        resp = self.client.get(self.base_url + '/', headers={'sid': '123'})
         self.assertEqual(resp.status_code, 503)
         self.assertIn('Mock answer for header!', resp.get_data(as_text=True))
 
-        resp = self.app.get(self.base_url + '/', headers={'sid': '345'})
+        resp = self.client.get(self.base_url + '/', headers={'sid': '345'})
         self.assertEqual(resp.status_code, 200)
         self.assertIn('Mock answer without header!', resp.get_data(as_text=True))
 
@@ -165,15 +156,14 @@ class FlamockTest(unittest.TestCase):
             'forward': {
                 'scheme': fwd_scheme,
                 'host': fwd_host
-            },
-            'priority': 0
+            }
         }
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_fwd))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_fwd))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.get(self.base_url + '/?text=Hello')
+        resp = self.client.get(self.base_url + '/?text=Hello')
         self.assertEqual(resp.status_code, 200)
         self.assertIn('text=Hello', resp.get_data(as_text=True))
 
@@ -182,34 +172,36 @@ class FlamockTest(unittest.TestCase):
         fwd_scheme = 'https'
 
         exp_fwd = {
+            'request': {
+                'path': 'search'
+            },
             'forward': {
                 'scheme': fwd_scheme,
                 'host': fwd_host,
                 'headers': {
                     'Host': 'yandex.ru'
                 }
-            },
-            'priority': 0
+            }
         }
 
-        resp = self.app.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
-                             data=json.dumps(exp_fwd))
+        resp = self.client.post(self.base_url + '/' + flamock_admin_path + '/add_expectation',
+                                data=json.dumps(exp_fwd))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.app.get(self.base_url + '/?text=Hello')
+        resp = self.client.get(self.base_url + '/?text=Hello')
         self.assertEqual(resp.status_code, 200)
         self.assertIn('text=Hello', resp.get_data(as_text=True))
 
     def test_070_get_empty_logs(self):
-        resp = self.app.get(self.base_url + '/' + flamock_admin_path + '/logs')
+        resp = self.client.get(self.base_url + '/' + flamock_admin_path + '/logs')
 
         self.assertEqual(resp.status_code, 200)
         self.assertEquals('{}', resp.get_data(as_text=True))
 
     def test_080_get_all_logs(self):
-        self.app.get(self.base_url + '/')
+        self.client.get(self.base_url + '/')
 
-        resp = self.app.get(self.base_url + '/' + flamock_admin_path + '/logs')
+        resp = self.client.get(self.base_url + '/' + flamock_admin_path + '/logs')
 
         self.assertEqual(resp.status_code, 200)
         resp_text = resp.get_data(as_text=True)
@@ -220,9 +212,9 @@ class FlamockTest(unittest.TestCase):
         self.assertIn('No expectation for request', resp_text)
 
     def test_090_get_log_by_id(self):
-        self.app.get(self.base_url + '/')
+        self.client.get(self.base_url + '/')
 
-        resp = self.app.get(self.base_url + '/' + flamock_admin_path + '/logs/0')
+        resp = self.client.get(self.base_url + '/' + flamock_admin_path + '/logs/0')
 
         self.assertEqual(resp.status_code, 200)
         resp_text = resp.get_data(as_text=True)
